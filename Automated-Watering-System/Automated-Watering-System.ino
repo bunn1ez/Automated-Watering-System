86c1a4d05dfcac3cd4b6989fecf2f23fec54fcc4
Bunn1ez <128931852+bunn1ez@users.noreply.github.com>
2023-03-27 00:31:02 -0400

    Update Automated-Watering-System.ino

--- Automated-Watering-System/Automated-Watering-System.ino
+++ Automated-Watering-System/Automated-Watering-System.ino
@@ -1,35 +1,3 @@
-/*********************************************************************************
- * This program is free software: you can redistribute it and/or modify        *
- * it under the terms of the GNU General Public License as published by          *
- * the Free Software Foundation, either version 2 of the License, or             *
- * (at your option) any later version.                                            *
- *                                                                               *
- * This program is distributed in the hope that it will be useful,               *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of                *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
- * GNU General Public License for more details.                                  *
- *                                                                               *
- * You should have received a copy of the GNU General Public License             *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.         *
- *********************************************************************************/
-
-/****************************************************************************************************************************************
-The parts used in this project are:
-1.  QAPASS LiquidCrystal_I2C
-2.  Relay: Songle SRD-05VDC-SL-C
-3.  Arduino Uno R3
-4.  Capacitive Soil Moisture Sensor v1.2, with values 250-550 [(250 = Very Wet) & (550 = Usually in open air)]
-5.  Pump: 5VDC 0.3A, Submersible Water Pump, for this project 120VAC is fed to a DC power inverter that turns 120VAC to 5VDC
-6.  Jumper Wires & male and female wire adapters
-7.  120VAC Cord: For SRD-05VDC-SL-C, Relay
-8.  120VAC Power from outlet
-9.  5VDC 0.5mA Power Supply: For Arduino Uno
-10. 5/16" Rubber Tubing, around 2-3 feet long
-11. One ANPTGHT 1/2" Hose Barb Fitting Equal Barbed Y Shaped 3 Way Plastic Tubing Joint Splicer: 
-    This is for the circle that will be created from the rubber tubing to water the plants and feed to the pump
-Note: For the LCD_I2C, make sure that the ~A5 pin is matched with SCL on the LCD_I2C, and the ~A4 pin is matched with the SDA pin on the LCD_I2C
-*****************************************************************************************************************************************/
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
